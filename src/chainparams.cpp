c3f883923b6fe77567707540280bcd213f819681
stevecat <38344452+minblock@users.noreply.github.com>
2020-04-09 20:48:13 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,11 +120,8 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0xff68d85c88518a90b32439c5a6c977c979e098265f0189dbe29c9af57f17b9a9"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        //vSeeds.push_back(CDNSSeedData("loshan.co.uk", "seed-a.freecoin.loshan.co.uk", true));
-        //vSeeds.push_back(CDNSSeedData("thrasher.io", "dnsseed.thrasher.io", true));
-        //vSeeds.push_back(CDNSSeedData("freecointools.com", "dnsseed.freecointools.com"));
-        //vSeeds.push_back(CDNSSeedData("freecoinpool.org", "dnsseed.freecoinpool.org"));
-        //vSeeds.push_back(CDNSSeedData("koin-project.com", "dnsseed.koin-project.com"));
+        vSeeds.push_back(CDNSSeedData("provigen.net", "seed.pgn.one", true));
+
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,79);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
